96468ae1f30e186e0ee10298b5944352c7a78490
valarnin <valarnin@gmail.com>
2024-08-01 01:29:48 -0400

    Adjustments per review

--- ui/raidboss/data/07-dt/raid/r3s.ts
+++ ui/raidboss/data/07-dt/raid/r3s.ts
@@ -1,27 +1,33 @@
 import Conditions from '../../../../../resources/conditions';
+import Outputs from '../../../../../resources/outputs';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
 
-export type Data = RaidbossData;
-
-const headMarkerData = {
-  // Offsets: 15872,172895
-  // Vfx Path: com_share6_5s0c
-  tankBuster: '01D3',
-} as const;
+export interface Data extends RaidbossData {
+  phaseTracker: number;
+}
 
 const triggerSet: TriggerSet<Data> = {
   id: 'AacLightHeavyweightM3Savage',
   zoneId: ZoneId.AacLightHeavyweightM3Savage,
   timelineFile: 'r3s.txt',
+  initData: () => ({
+    phaseTracker: 0,
+  }),
   triggers: [
     {
-      id: 'R3S Headmarker Tankbuster',
-      type: 'HeadMarker',
-      netRegex: { id: headMarkerData.tankBuster, capture: true },
-      response: Responses.tankBuster(),
+      id: 'R3S Phase Tracker',
+      type: 'GainsEffect',
+      netRegex: { effectId: 'FB6', capture: true },
+      run: (data, matches) => data.phaseTracker = parseInt(matches.count, 16),
+    },
+    {
+      id: 'R3S Knuckle Sandwich',
+      type: 'StartsUsing',
+      netRegex: { id: '9423', source: 'Brute Bomber' },
+      response: Responses.sharedTankBuster(),
     },
     {
       id: 'R3S Brutal Impact',
@@ -77,7 +83,17 @@ const triggerSet: TriggerSet<Data> = {
       id: 'R3S Murderous Mist',
       type: 'StartsUsing',
       netRegex: { id: '93FE', source: 'Brute Bomber', capture: false },
-      response: Responses.getBehind(),
+      infoText: (data, _matches, output) => {
+        if (data.phaseTracker > 0)
+          return output.getHit!();
+        return output.getBehind!();
+      },
+      outputStrings: {
+        getBehind: Outputs.getBehind,
+        getHit: {
+          en: 'Get hit by mist',
+        },
+      },
     },
     {
       id: 'R3S Quadroboom Dive Proximity',
@@ -143,6 +159,25 @@ const triggerSet: TriggerSet<Data> = {
       infoText: (_data, _matches, output) => output.text!(),
       outputStrings: {
         text: {
+          en: 'Long Fuse',
+        },
+      },
+    },
+    {
+      id: 'R3S Fuse Field',
+      type: 'GainsEffect',
+      netRegex: { effectId: 'FB4' },
+      condition: Conditions.targetIsYou(),
+      alertText: (_data, matches, output) => {
+        if (parseFloat(matches.duration) < 30)
+          return output.short!();
+        return output.long!();
+      },
+      outputStrings: {
+        short: {
+          en: 'Short Fuse',
+        },
+        long: {
           en: 'Long Fuse',
         },
       },
